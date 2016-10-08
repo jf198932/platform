dfee092848856867d72b0a941d2ca92ed0bfb833
jinfeng0 <458166388@qq.com>
2016-10-08 16:02:36 +0800

    WebApi

---
+++ isriding.WebApi/isridingWebApiModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Abp.Application.Services;
+using Abp.Configuration.Startup;
+using Abp.Modules;
+using Abp.WebApi;
+
+namespace isriding
+{
+    [DependsOn(typeof(AbpWebApiModule), typeof(isridingApplicationModule))]
+    public class isridingWebApiModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            Configuration.Modules.AbpWebApi().DynamicApiControllerBuilder
+                .ForAll<IApplicationService>(typeof(isridingApplicationModule).Assembly, "app")
+                .Build();
+        }
+    }
+}
