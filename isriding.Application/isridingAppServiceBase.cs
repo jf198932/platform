2cdb48424ac7ad7d31c10a88aad652dbf236b231
jinfeng0 <458166388@qq.com>
2016-10-08 15:57:52 +0800

    Application

---
+++ isriding.Application/isridingAppServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+
+namespace isriding
+{
+    /// <summary>
+    /// Derive your application services from this class.
+    /// </summary>
+    public abstract class isridingAppServiceBase : ApplicationService
+    {
+        protected isridingAppServiceBase()
+        {
+            LocalizationSourceName = isridingConsts.LocalizationSourceName;
+        }
+    }
+}
---
+++ isriding.Application/isridingApplicationModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Abp.Modules;
+
+namespace isriding
+{
+    [DependsOn(typeof(isridingCoreModule))]
+    public class isridingApplicationModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
