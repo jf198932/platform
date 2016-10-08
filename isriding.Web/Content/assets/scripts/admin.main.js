4a51f4373d47a6a34a4949988d9334cec189dae7
jinfeng0 <458166388@qq.com>
2016-10-08 16:04:05 +0800

    Content/assets

---
+++ isriding.Web/Content/assets/scripts/admin.main.js
@@ -0,0 +1,137 @@
+﻿//选择主题及初始化主题逻辑
+//(function () {
+//    $(".theme-colors > ul > li").click(function () {
+//        var color = $(this).attr("data-style");
+//        $.cookie('currentTheme', color, { expires: 7, path: '/' });
+//    });
+//    var currentTheme = $.cookie('currentTheme');
+//    if (currentTheme != null && currentTheme) {
+//        $('#style_color').attr("href", "/administration/content/assets/css/themes/" + currentTheme + ".css");
+//    }
+//})();
+
+//表单验证
+(function () {
+    $(".form-horizontal .field-validation-error").each(function () {
+        if ($(this).html() != "") {
+            $(this).parent().parent().parent().addClass("has-error");
+        }
+    });
+})();
+
+////TODO:新菜单根据Url决定逻辑, 目前适配二级菜单
+//新菜单根据Url决定逻辑, 目前适配多级菜单
+(function () {
+    var locationHref = window.location.href.toLocaleLowerCase();
+    var strhtml = "";
+    var index = 0;
+    $(".page-sidebar-menu > li > a").each(function () {
+        var parent = $(this);
+        //sideMenus(locationHref, parent, strhtml, index);
+        
+        $(this).next("ul").each(function () {
+            $("a", $(this)).each(function () {
+                if (locationHref.indexOf($(this).attr("key")) > 0) {
+                    var htmlTemplate = new Object();
+                    sideMenus($(this).parent(), $(this), 0, htmlTemplate);
+                    
+                    var breads = htmlTemplate.con.split("<jf>");
+                    var breadstr = "";
+                    for (var i = breads.length ; i > 0; i--) {
+                        breadstr += breads[i - 1];
+                    }
+                    $("#navigation .breadcrumb").append(breadstr);
+                    
+                    document.title = $(this).text() + " - " + document.title;
+
+                    return false;
+                }
+            });
+        });
+        if (locationHref.indexOf($(this).attr("key")) > 0) {
+            $(this).parent().addClass("active");
+            $(this).append("<span class='selected'></span>");
+
+            $("#navigation .page-title span").html($(this).text());
+            $("#navigation .page-title small").html($(this).attr("title") || "");
+
+            var htmlTemplate = "";            
+            htmlTemplate += "<li>";
+            htmlTemplate += "<i class='" + $(this).find("i").attr("class") + "'>" + "</i>";
+            htmlTemplate += "<a href='" + $(this).attr("href") + "'>" + $(this).attr("title") + "</a>";
+            htmlTemplate += "</li>";
+            $("#navigation .breadcrumb").append(htmlTemplate);
+
+            document.title = $(this).text() + " - " + document.title;
+            return false;
+        }
+    });
+})();
+
+function sideMenus(objli, obja, i, htmlTemplate) {
+    objli.addClass("active");
+    $(".arrow", objli).addClass("open")    
+    //$(".arrow", objli).addClass("open").before("<span class='selected'></span>");
+    if (i == 0) {
+        htmlTemplate.con = "<li>";
+        htmlTemplate.con += "<i class='" + obja.find("i").attr("class") + "'>" + "</i>";
+        htmlTemplate.con += "<a href='" + obja.attr("href") + "'>" + obja.attr("title") + "</a>";
+        htmlTemplate.con += "</li>";
+        htmlTemplate.con += "<jf>";
+        $("#navigation .page-title span").html(obja.text());
+        $("#navigation .page-title small").html(obja.attr("title") || "");
+    } else {
+        htmlTemplate.con += "<li>";
+        htmlTemplate.con += "<i class='" + $("a", objli).find("i").attr("class") + "'>" + "</i>";
+        htmlTemplate.con += "<span>" + $("a", objli).attr("title") + "</span>";
+        htmlTemplate.con += "<i class='fa fa-angle-right'></i>";
+        htmlTemplate.con += "</li>";
+        htmlTemplate.con += "<jf>"
+    }
+    i++;
+    if (objli.parent()[0].className === "page-sidebar-menu") {
+        $(".arrow", objli).before("<span class='selected'></span>");
+    } else {
+        sideMenus(objli.parent().parent(), obja, i, htmlTemplate);
+    }    
+}
+
+(function () {
+    var isIE8Or9 = false;
+
+    if (window.ActiveXObject) {
+        var ua = navigator.userAgent.toLowerCase();
+        var ie = ua.match(/msie ([\d.]+)/)[1]
+        if (ie == 8.0 || ie == 9.0) {
+            isIE8Or9 = true;
+        }
+
+        if (ie == 6.0) {
+            alert("您的浏览器版本是IE6，在本系统中不能达到良好的视觉效果，建议你升级到IE8及以上！")
+        }
+    }
+
+    if (!isIE8Or9) {
+        //alert("您的浏览器版本不是IE8或IE9，在本系统中不能达到良好的视觉效果，建议你升级到IE8以上！")
+    }
+})();
+
+$("#checkall").click(function () {
+    var ischecked = this.checked;
+    $("input:checkbox[name='ids']").each(function () {
+        this.checked = ischecked;
+    });
+
+    $.uniform.update(':checkbox');
+});
+
+$("#delete").click(function () {
+    var message = "你确定要删除勾选的记录吗?";
+    if ($(this).attr("message"))
+        message = $(this).attr("message") + "，" + message;
+    if (confirm(message))
+        $("#mainForm").submit();
+});
+
+
+
---
+++ isriding.Web/Content/assets/scripts/table-managed.js
@@ -0,0 +1,175 @@
+﻿//JQuery DataTable
+var TableManaged = {
+    oTable: '',
+    iDisplayLength : 15,
+
+    init: function (dataTableObj, actionUrl, aoColumns, pageSize) {
+        
+        if (!jQuery().dataTable) {
+            return;
+        }
+
+        if (typeof (pageSize) != 'undefined') {
+            this.iDisplayLength = pageSize;
+        }
+
+        this.oTable = dataTableObj.dataTable({
+            'bLengthChange': false,
+            "bFilter": false,
+            "bSort": false,
+            "bPaginate": true,
+            "bServerSide": true,
+            "bStateSave": false,
+            "iDisplayLength": this.iDisplayLength,
+            "sPaginationType": "bootstrap",
+            "aLengthMenu": [[10, 20, 50, 100], [10, 20, 50, 100]],
+            "oLanguage": {
+                "sLengthMenu": "每页显示 _MENU_ 条记录",
+                "sZeroRecords": "对不起，查询不到任何相关数据",
+                "sInfo": "当前显示 _START_ 到 _END_ 条，共 _TOTAL_ 条记录",
+                "sInfoEmtpy": "找不到相关数据",
+                "sInfoFiltered": "数据表中共为 _MAX_ 条记录",
+                "sProcessing": "正在加载中...",
+                "sSearch": "搜索",
+                "oPaginate": {
+                    "sFirst": "第一页",
+                    "sPrevious": " 上一页 ",
+                    "sNext": " 下一页 ",
+                    "sLast": " 最后一页 "
+                }
+            },
+            "sAjaxSource": actionUrl,
+            "aoColumns": aoColumns,
+            "aoColumnDefs": [{
+                'bSortable': false,
+                'aTargets': [0]
+            }]
+        });
+        return this.oTable;        
+    },
+
+    search: function (searchUrl) {
+        var filterdata = $("#search_form").serialize();
+        var actionUrl = searchUrl + "?rand=" + Math.random() + "&" + filterdata;
+        this.oTable.fnReloadAjax(actionUrl);
+    },
+    //清除表单数据
+    reset:function(obj){
+        obj.find(':input').not(':button, :submit, :reset').val('').removeAttr('checked').removeAttr('selected');
+        var slist = obj.find('select');
+        for (var i = 0; i < slist.length; i++) {
+            if (slist[i].options.length > 0) {
+                slist[i].selectedIndex = 0;
+            }
+        }
+    },
+    //编辑
+    btnEdit: function (actionUrl, id) {
+        return "<a href=\"javascript:void(0);\" class=\"btn btn-xs purple\" onclick=\"Edit('"+actionUrl+"','"+id+"')\"><i class=\"fa fa-edit\"></i> 编辑</a>";
+    },
+    //删除
+    btnDelete: function (actionUrl, id) {
+        return "<a href=\"javascript:void(0)\" class=\"btn btn-xs red\" onclick=\"TableManaged.confirmDelete(this, '" + actionUrl + "', '"+ id +"')\"><i class=\"fa fa-times\"></i> 删除</a>";
+    },
+
+    confirmDelete: function (target, actionUrl, id) {
+        if (!confirm("你确认要删除此记录？")) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: actionUrl,
+            data: {id : id},
+            success: function(response) {
+                if (response.success) {
+                    var nRow = $(target).parents('tr')[0];
+                    TableManaged.oTable.fnDeleteRow(nRow);
+                } else {
+                    alert(response.message);
+                }
+
+            }
+        });
+
+    }
+};
+
+$('.datatable .group-checkable').change(function () {
+    var set = jQuery(this).attr("data-set");
+    var checked = jQuery(this).is(":checked");
+    jQuery(set).each(function () {
+        if (checked) {
+            $(this).attr("checked", true);
+        } else {
+            $(this).attr("checked", false);
+        }
+    });
+    jQuery.uniform.update(set);
+});
+
+/*******弹出表单*********/
+function ShowModal(actionUrl, param, title) {
+    $("#modal-form").draggable({
+        cursor: "move",
+        handle: ".modal-header"
+    });
+    //表单初始化
+    $(".modal-title").html(title);
+    $("#modal-content").attr("action", actionUrl);
+
+    $.ajax({
+        type: "GET",
+        url: actionUrl,
+        data: param,
+        beforeSend: function () {
+            //
+        },
+        success: function (result) {
+            $("#modal-content").html(result);
+            $('#modal-form').modal('show');
+            RegisterForm();
+        },
+        error: function () {
+            //
+        },
+        complete: function () {
+            //
+        }
+    });
+}
+
+/*******保存表单*********/
+function SaveModal(oTable) {
+    var actionUrl = $("#modal-content").attr("action");
+    var $form = $("#modal-content");
+
+    if (!$form.valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: actionUrl,
+        data: $form.serialize(),
+        success: function (result) {
+            //判断返回值，若为Object类型，即操作成功
+            //var result = ((typeof data == 'object') && (data.constructor == Object));
+            if (result != null) {
+                //bootbox.alert(data.Message);
+                $('#modal-form').modal('hide');
+                oTable.fnDraw();
+            }
+            else {
+                $("#modal-content").html(data);
+            }
+        }
+    });
+}
+
+/*******注册验证脚本*********/
+function RegisterForm() {
+    $('#modal-content').removeData('validator');
+    $('#modal-content').removeData('unobtrusiveValidation');
+    $.validator.unobtrusive.parse('#modal-content');
+}
